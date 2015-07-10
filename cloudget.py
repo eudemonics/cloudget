876a5be0dad2dfac19dce27194674357308872d0
eudemonics <vvn@eudemonics.org>
2015-07-10 13:36:16 -0700

    added link harvesting and following. cURL still broken

--- cloudget.py
+++ cloudget.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-# cloudget v0.1
+# cloudget v0.2
+# release date: July 10, 2015
 # author: vvn < lost @ nobody . ninja >
 
-import sys, getopt, subprocess, os
+import sys, getopt, subprocess, os, re, random, string, time
+from urlparse import urlparse
+from subprocess import PIPE, check_output, Popen
 try:
    import cfscrape
 except:
@@ -13,113 +16,194 @@ except:
    except:
       print('unable to install the cfscrape module via pip. this script requires cfscrape to run. get it here: https://github.com/Anorov/cloudflare-scrape')
       sys.exit(1)
+      
+
+print('''
+=====================================
+----------- CLOUDGET v0.2 -----------
+-------------------------------------
+----------- author : vvn ------------
+--------- lost@nobody.ninja ---------
+=====================================
+---- support my work: buy my EP! ----
+-------- http://dreamcorp.us --------
+--- facebook.com/dreamcorporation ---
+------ thanks for the support! ------
+=====================================
+''')
+
+try:
+   from BeautifulSoup import BeautifulSoup
+except:
+   pass
+   try:
+      os.system('pip install BeautifulSoup')
+      from BeautifulSoup import BeautifulSoup
+   except:
+      print('BeautifulSoup module is required to run the script.')
+      sys.exit(1)
 
 global cfurl
 global usecurl
 global writeout
 global curldl
+global links
 usecurl = 0
 writeout = 0
+links = 0
 
 try:
-   opts, args = getopt.getopt(sys.argv[1:],"hu:oc",["help","url=","out","curl"])
-   
+   opts, args = getopt.getopt(sys.argv[1:],"hu:ocl",["help","url=","out","curl","links"])
+
 except getopt.GetoptError:
-   print('cloudget.py [-c] [-o] -u <url> \n')
+   print('cloudget.py [-c] [-o] [-l] -u <url> \n')
    print('-c, --curl: use cURL')
    print('-o, --out: save output')
+   print('l, --links: harvest links')
    print('-u, --url <url>: cloudflare URL to get [REQUIRED]')
    sys.exit(2)
 
 for opt, arg in opts:
    if opt in ("-h", "--help"):
-      print('cloudget.py [-c] [-o] -u <url> \n')
+      print('cloudget.py [-c] [-o] [-l] -u <url> \n')
       print('-c, --curl: use cURL without downloading')
       print('-o, --out: save output')
+      print('l, --links: harvest links')
       print('-u, --url <url>: cloudflare URL to get [REQUIRED]')
-      sys.exit()
+
    elif opt in ("-o", "--out"):
       writeout = 1
    elif opt in ("-u", "--url"):
       cfurl = arg
    elif opt in ("-c", "--curl"):
       usecurl = 1
+   elif opt in ("-l", "--links"):
+      links = 1
 
-checkcurl = ''
+def getCF(cfurl):
 
-if usecurl == 1:
-   checkcurl = 'yes'
-else:
-   checkcurl = 'no'
-print("using curl: %s \n" % checkcurl)
+   checkcurl = ''
 
-if writeout == 1:
-   global outfile
-   outfile = cfurl.split('/')[-1]
-   print("output file: %s \n" % outfile)
+   if usecurl == 1:
+      checkcurl = 'yes'
+   else:
+      checkcurl = 'no'
+   print("using curl: %s \n" % checkcurl)
 
-print("creating new session..\n")
-scraper = cfscrape.create_scraper() # returns a requests.Session object
 
-try:
-   if usecurl == 1 and writeout == 1:
-      r = scraper.get(cfurl, stream=True)
-      print("status: ")
-      print(r.status_code)
-      print("\nheaders: ")
-      print(r.headers)
-      print("\nfetching cookies for %s.. \n" % cfurl)
-      cookie_arg = cfscrape.get_cookie_string(cfurl)
-      print("trying to download using cURL to %s.. \n" % outfile)
-      command_text = 'curl -# --no-keepalive -L --cookie ' + cookie_arg + ' -O ' + cfurl
-      output = Popen(command_text, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
-      response, errors = output.communicate()
-      print("response: " + str(response))
-      print("errors: " + str(errors))
-   
-   elif usecurl == 1 and writeout == 0:
-      print("status: ")
-      print(scraper.get(cfurl).status_code)
-      print("\nheaders: ")
-      print(scraper.get(cfurl).headers)
-      print("getting cookies for url: %s \n" % cfurl)
-      cookie_arg = cfscrape.get_cookie_string(cfurl)
-      print(cookie_arg)
-      print("checking cURL output...\n")
-      result = subprocess.check_output(["curl", "--cookie", cookie_arg, "-L", "--no-keepalive", cfurl])
-      print(result)
-   
-   elif usecurl == 0 and writeout == 1:
-      print("getting %s... \n" % cfurl)
-      r = scraper.get(cfurl, stream=True)
-      print("status: ")
-      print(r.status_code)
-      print("\nheaders: ")
-      print(r.headers)
+   if writeout == 1:
+      global outfile
+      outfile = cfurl.split('/')[-1]
+      if len(outfile) < 1:
+         outfile = cfurl.lstrip('http:').strip('/') + '.html'
+      print("output file: %s \n" % outfile)
       if not os.path.exists('download'):
          os.makedirs('download')
-      savefile = os.path.join('download', outfile)
-      print("\nsaving content to %s \n" % savefile)
+      outfile = os.path.join('download', outfile)
 
-      with open(savefile, 'wb+') as dlfile:
-         for chunk in r.iter_content(chunk_size=4096):
-            if chunk:
-               dlfile.write(chunk)
-               dlfile.flush()
-               os.fsync(dlfile.fileno())
-         dlfile.close()
-      print("file saved! \n")
-      
-   else:
-      print("getting %s... \n" % cfurl)
-      print(scraper.get(cfurl).content)
+   print("creating new session..\n")
+   scraper = cfscrape.create_scraper() # returns a requests.Session object
 
-except Exception, e:
-   print("an error has occurred: %s \n" % str(e))
-   sys.exit(1)
+   try:
+      if usecurl == 1 and writeout == 1:
+         r = scraper.get(cfurl, stream=True)
+         print("status: ")
+         print(r.status_code)
+         print("\nheaders: ")
+         print(r.headers)
+         print("\ngetting cookies for %s.. \n" % cfurl)
+         cookie_arg = cfscrape.get_cookie_string(cfurl)
+         print("trying to download using cURL to %s.. \n" % outfile)
+         command_text = 'curl -# --no-keepalive -L -O --cookie ' + cookie_arg + ' ' + cfurl
+         print(command_text)
+         output = Popen(command_text, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+         response, errors = output.communicate()
+         print("\nresponse: " + str(response))
+         print("\nerrors: " + str(errors))
 
-finally:   
-   print("exiting.. \n")
-   sys.exit(0)
+      elif usecurl == 1 and writeout == 0:
+         r = scraper.get(cfurl, stream=True)
+         print("status: ")
+         print(r.status_code)
+         print("\nheaders: ")
+         print(r.headers)
+         print("\ngetting cookies for url: %s \n" % cfurl)
+         cookie_arg = cfscrape.get_cookie_string(cfurl)
+         print(cookie_arg)
+         print("\nchecking cURL output...\n")
+         result2 = check_output(["curl", "-#", "--cookie", cookie_arg, "-L", "-k", "--no-keepalive", cfurl])
+         time.sleep(5)
+         result = check_output(["curl", "-#", "--cookie", cookie_arg, "-L", "-k", "--no-keepalive", cfurl])
+         print(result)
+
+      elif usecurl == 0 and writeout == 1:
+         print("getting %s... \n" % cfurl)
+         r = scraper.get(cfurl, stream=True)
+         print("status: ")
+         print(r.status_code)
+         print("\nheaders: ")
+         print(r.headers)
+         print("\nsaving content to %s \n" % outfile)
+
+         with open(outfile, 'wb+') as dlfile:
+            for chunk in r.iter_content(chunk_size=4096):
+               if chunk:
+                  dlfile.write(chunk)
+                  dlfile.flush()
+                  os.fsync(dlfile.fileno())
+            dlfile.close()
+         print("file saved! \n")
+
+      else:
+         print("getting %s... \n" % cfurl)
+         #print(scraper.get(cfurl).content)
+         r = scraper.get(cfurl, stream=True)
+         html = BeautifulSoup(r.text)
+         bs = html.prettify()
+         print(bs)
+
+      def getlinks(cfurl):
+         r = scraper.get(cfurl, stream=True)
+         html = BeautifulSoup(r.text)
+         bs = html.prettify()
+         print('\nFOUND LINKS:\n')
+         for link in html.findAll('a'):
+            b = link.get('href')
+            b = str(b)
+            print(b)
+         print('')
+         follow = raw_input('fetch harvested links? enter Y/N --> ')
+         while not re.search(r'^[yYnN]$', follow):
+            follow = raw_input('invalid entry. enter Y to follow harvested links or N to quit --> ')
+         
+         if follow.lower() == 'y':
+            p = urlparse(cfurl)
+            part = p.path.split('/')[-1]
+            path = p.path.strip(part)
+            parent = p.scheme + '://' + p.netloc + path
+            for link in html.findAll('a'):
+               b = link.get('href')
+               if not re.match(r'^(\.\.\/)$', str(b)):
+                  b = parent + b
+                  print("requesting harvested URL: %s \n" % b)
+                  links == 0
+                  getCF(b)
+                  getlinks(b)
+         else:
+            sys.exit(0)
+
+      if links == 1:
+         getlinks(cfurl)
+
+   except Exception, e:
+      print("an error has occurred: %s \n" % str(e))
+      raise Exception
+      sys.exit(1)
+
+   finally:
+      print("exiting.. \n")
+      sys.exit(0)
+
+getCF(cfurl)
 
 sys.exit()
