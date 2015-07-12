7e2e499ebee742b429131f2ec5d2b8057a9d1d99
eudemonics <vvn@eudemonics.org>
2015-07-11 20:52:46 -0700

    updated from getopt to argparse, added download progress bar

--- cloudget.py
+++ cloudget.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# cloudget v0.2
-# release date: July 10, 2015
+# cloudget v0.3
+# release date: July 11, 2015
 # author: vvn < lost @ nobody . ninja >
 
-import sys, getopt, subprocess, os, re, random, string, time
+import sys, argparse, subprocess, os, re, random, string, time
 from urlparse import urlparse
 from subprocess import PIPE, check_output, Popen
 try:
@@ -18,9 +18,9 @@ except:
       sys.exit(1)
 
 
-print('''
+intro = '''
 =====================================
------------ CLOUDGET v0.2 -----------
+----------- CLOUDGET v0.3 -----------
 -------------------------------------
 ----------- author : vvn ------------
 --------- lost@nobody.ninja ---------
@@ -30,7 +30,9 @@ print('''
 --- facebook.com/dreamcorporation ---
 ------ thanks for the support! ------
 =====================================
-''')
+'''
+
+print(intro)
 
 try:
    from BeautifulSoup import BeautifulSoup
@@ -46,41 +48,33 @@ except:
 global cfurl
 global usecurl
 global writeout
-global curldl
 global links
 usecurl = 0
 writeout = 0
 links = 0
 
-try:
-   opts, args = getopt.getopt(sys.argv[1:],"hu:ocl",["help","url=","out","curl","links"])
+parser = argparse.ArgumentParser(description="a script to automatically bypass anti-robot measures and download links from servers behind a cloudflare proxy")
+ 
+parser.add_argument('-u', '--url', action='store', help='[**REQUIRED**] full cloudflare URL to retrieve, beginning with http(s)://', required=True)
+parser.add_argument('-o', '--out', help='save output to \'download\' directory', action='store_true', required=False)
+parser.add_argument('-l', '--links', help='harvest links found in response', action='store_true', required=False)
+parser.add_argument('-c', '--curl', nargs='?', default='empty', const='curl', dest='curl', metavar='CURL_OPTS', help='use cURL. use %(metavar)s to pass optional cURL parameters. (for more info try \'curl --manual\')', required=False)
+parser.add_argument('--version', action='version', version='%(prog)s 0.3 by vvn <lost@nobody.ninja>')
 
-except getopt.GetoptError:
-   print('cloudget.py [-c] [-o] [-l] -u <url> \n')
-   print('-c, --curl: use cURL')
-   print('-o, --out: save output')
-   print('l, --links: harvest links')
-   print('-u, --url <url>: cloudflare URL to get [**REQUIRED**]')
-   print('**USE COMPLETE URL beginning with http://**')
-   sys.exit(2)
-
-for opt, arg in opts:
-   if opt in ("-h", "--help"):
-      print('cloudget.py [-c] [-o] [-l] -u <url> \n')
-      print('-c, --curl: use cURL without downloading')
-      print('-o, --out: save output')
-      print('l, --links: harvest links')
-      print('-u, --url <url>: cloudflare URL to get [**REQUIRED**]')
-      print('**USE COMPLETE URL beginning with http://**')
-
-   elif opt in ("-o", "--out"):
-      writeout = 1
-   elif opt in ("-u", "--url"):
-      cfurl = arg
-   elif opt in ("-c", "--curl"):
-      usecurl = 1
-   elif opt in ("-l", "--links"):
-      links = 1
+args = parser.parse_args()
+if args.out:
+   writeout = 1
+if args.links:
+   links = 1
+if args.curl in 'empty':
+   usecurl = 0
+elif args.curl is 'curl':
+   usecurl = 1
+else:
+   usecurl = 1
+   global curlopts
+   curlopts = args.curl
+cfurl = args.url
 
 def getCF(cfurl):
 
@@ -91,16 +85,27 @@ def getCF(cfurl):
    else:
       checkcurl = 'no'
    print("using curl: %s \n" % checkcurl)
+   
+   if links == 1:
+      checklinks = 'yes'
+   else:
+      checklinks = 'no'
+   print("harvesting links: %s \n" % checklinks)
 
    if writeout == 1:
       global outfile
-      outfile = cfurl.split('/')[-1]
+      p = urlparse(cfurl)
+      outfile = cfurl.split('?')[0]
+      outfile = outfile.split('/')[-1]
       if len(outfile) < 1:
          outfile = cfurl.lstrip('http:').strip('/') + '.html'
+      if p.netloc in outfile:
+         outfile = outfile + '.html'
       print("output file: %s \n" % outfile)
       if not os.path.exists('download'):
          os.makedirs('download')
-      outfile = os.path.join('download', outfile)
+      global savefile
+      savefile = os.path.join('download', outfile)
 
    print("creating new session..\n")
    scraper = cfscrape.create_scraper() # returns a requests.Session object
@@ -114,8 +119,11 @@ def getCF(cfurl):
          print(r.headers)
          print("\ngetting cookies for %s.. \n" % cfurl)
          cookie_arg = cfscrape.get_cookie_string(cfurl)
-         print("trying to download using cURL to %s.. \n" % outfile)
-         command_text = 'curl -# --no-keepalive --ignore-content-length -L -O --cookie \'' + cookie_arg + '\' ' + cfurl
+         print(cookie_arg)
+         print("\ntrying to download using cURL to %s.. \n" % outfile)
+         command_text = 'curl -# --no-keepalive --ignore-content-length -L -k -O --cookie \'' + cookie_arg + '\' ' + cfurl
+         if 'curlopts' in locals():
+            command_text = 'curl -# --cookie \'' + cookie_arg + '\' ' + curlopts + ' --no-keepalive -O ' + cfurl
          print(command_text)
          output = subprocess.Popen(command_text, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
          response, errors = output.communicate()
@@ -132,10 +140,16 @@ def getCF(cfurl):
          cookie_arg = cfscrape.get_cookie_string(cfurl)
          print(cookie_arg)
          print("\nchecking cURL output...\n")
-         result1 = check_output(["curl", "-#", "--cookie", cookie_arg, cfurl])
-         time.sleep(5)
-         result = check_output(["curl", "-#", "--cookie", cookie_arg, "-L", "-k", "--no-keepalive", cfurl])
-         print(result)
+         if 'curlopts' in locals():
+            command_text = 'curl -# --cookie \'' + cookie_arg + '\' ' + curlopts + ' --no-keepalive -O ' + cfurl
+            output = subprocess.Popen(command_text, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+            response, errors = output.communicate()
+            print("\nresponse: %s \n" % str(response))
+         else:
+            result1 = check_output(["curl", "-#", "--cookie", cookie_arg, cfurl])
+            time.sleep(5)
+            result = check_output(["curl", "-#", "--cookie", cookie_arg, "-L", "-k", "--no-keepalive", cfurl])
+            print(result)
 
       elif usecurl == 0 and writeout == 1:
          print("getting %s... \n" % cfurl)
@@ -144,24 +158,44 @@ def getCF(cfurl):
          print(r.status_code)
          print("\nheaders: ")
          print(r.headers)
-         if re.search(r'^(\.htm[l]?|\.php|\.[aj]sp[x]?|\.cfm|\/)$',cfurl):
+         if re.search(r'(\.htm[l]?|\.php|\.[aj]sp[x]?|\.cfm|\/)$',cfurl):
             print(bs)
-         print("\nsaving content to %s. this may take awhile depending on file size... \n" % outfile)
-
-         with open(outfile, 'wb+') as dlfile:
-            for chunk in r.iter_content(chunk_size=2048):
-               if chunk:
-                  dlfile.write(chunk)
-                  dlfile.flush()
-                  os.fsync(dlfile.fileno())
+         print("\nsaving content to \'download\' directory as %s. this may take awhile depending on file size... \n" % outfile)
+         start = time.clock()
+         filesize = r.headers.get('Content-Length')
+         dld = 0
+         progress = lambda a: a - start
+         with open(savefile, 'wb+') as dlfile:
+            if filesize is None:
+               dlfile.write(r.content)
+            else:
+               print('\nfilesize: %s bytes \n' % str(filesize))
+               filesize = int(filesize)
+               for chunk in r.iter_content(chunk_size=2048):
+                  if chunk:
+                     dld += len(chunk)
+                     dlfile.write(chunk)
+                     done = int((50 * dld) / filesize)
+                     sys.stdout.write("\rdownloaded: %d bytes   [%s%s] %s kb/s" % (dld, '#' * done, ' ' * (50 - done), 0.000125 * (dld / (time.clock() - start))))
+                     dlfile.flush()
+                     os.fsync(dlfile.fileno())
             dlfile.close()
-         print("file saved! \n")
+         elapsed = time.clock() - start
+         print("\ntime elapsed: %s \n" % str(elapsed))
+         print("\nfile saved! \n")
 
       else:
          print("getting %s... \n" % cfurl)
          #print(scraper.get(cfurl).content)
          r = scraper.get(cfurl, stream=True)
-         html = BeautifulSoup(r.text)
+         print("status: ")
+         print(r.status_code)
+         print("\nheaders: ")
+         print(r.headers)
+         print('')
+         time.sleep(5)
+         s = scraper.get(cfurl, stream=True)
+         html = BeautifulSoup(s.text)
          bs = html.prettify()
          if re.search(r'^(.*)(\.html?|\.php|\.[aj]spx?|\.cfm)|(\/)$',cfurl):
             print(bs)
@@ -203,7 +237,7 @@ def getCF(cfurl):
                   b = link.get('href')
                   if not re.match(r'^(\.\.\/)$', str(b)):
                      b = parent + b
-                     print("requesting harvested URL: %s \n" % b)
+                     print("\nrequesting harvested URL: %s \n" % b)
                      getCF(b)
                      getlinks(b)
                   else:
@@ -213,14 +247,13 @@ def getCF(cfurl):
                   b = link.get('href')
                   if not re.search(r'^(.*)(\/)$', str(b)):
                      b = parent + b
-                     print("requesting harvested URL: %s \n" % b)
+                     print("\nrequesting harvested URL: %s \n" % b)
                      getCF(b)
-                     getlinks(b)
                   else:
                      continue
 
    except Exception, e:
-      print("an error has occurred: %s \n" % str(e))
+      print("\nan error has occurred: %s \n" % str(e))
       raise Exception
       sys.exit(1)
 
